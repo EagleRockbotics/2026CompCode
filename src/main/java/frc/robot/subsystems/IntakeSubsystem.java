f44538e7960e17e46e774aa105489987b288e291
henry <henryjordanemail@gmail.com>
2026-02-15 16:12:26 -0800

    Added constants regarding PID and ArmfeedForward; Added code to Intake regarding the rotate arm and intake motors

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,10 +12,16 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 import frc.robot.Constants;
+import swervelib.encoders.CanAndMagSwerve;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+
 
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -24,8 +30,12 @@ public class IntakeSubsystem extends SubsystemBase {
   private final SparkFlex m_motorBackIntake;
   private final SparkFlex m_motorBackSpin;
   private final XboxController m_driveController;
+  private final PIDController m_pid;
 
   private final SparkFlexConfig m_motorConfig = new SparkFlexConfig();
+
+  private final CANcoder m_encoder;
+  private final ArmFeedforward m_armFeed;
 
 
  
@@ -33,9 +43,11 @@ public class IntakeSubsystem extends SubsystemBase {
     m_motorFrontIntake = new SparkFlex(Constants.IntakeConstants.k_FrontIntakeId, null);
     m_motorFrontSpin = new SparkFlex(Constants.IntakeConstants.k_FrontSpinId, null);
     m_motorBackIntake = new SparkFlex(Constants.IntakeConstants.k_BackIntakeId, null);
-    m_motorBackSpin = new SparkFlex(Constants.IntakeConstants.k_BackSpinId, );
+    m_motorBackSpin = new SparkFlex(Constants.IntakeConstants.k_BackSpinId,null );
     m_driveController = new XboxController(0);
-  
+    m_pid = new PIDController(Constants.IntakeConstants.k_Kp, 0, Constants.IntakeConstants.k_Kd);
+    m_encoder = new CANcoder(0);
+    m_armFeed = new ArmFeedforward(Constants.IntakeConstants.k_Ks, Constants.IntakeConstants.k_Kg, Constants.IntakeConstants.k_Kv);
   }
   /**
    * Example command factory method.
@@ -47,31 +59,45 @@ public class IntakeSubsystem extends SubsystemBase {
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return run(
         () -> {
-          if (m_driveController.getBButton() && !m_driveController.getBButton()){
-            m_motorFrontSpin.set(1);
+          //Spin Motor Code
+          if (m_driveController.getBButton() && !m_driveController.getAButton()){
+            m_motorFrontSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/180));
+          
+            /*if (m_encoder.getPosition().getValueAsDouble() >= 0.25) {
+              m_motorFrontSpin.set(0);
+            }*/
           } else {
-            m_motorFrontSpin.set(0);
+            m_motorFrontSpin.set(calculateMotorOutput(0));
+                      }
+          /* 
+          if (m_driveController.getAButton() && !m_driveController.getBButton()) {
+            m_motorBackSpin.set(Constants.IntakeConstants.k_SpinPower);
+          } else {
+            m_motorBackSpin.set(-Constants.IntakeConstants.k_SpinPower);
+          }
+          */
+          if (m_driveController.getAButton() && !m_driveController.getBButton()){
+            m_motorBackSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/ 180));
+          } else {
+            m_motorBackSpin.set(calculateMotorOutput(0));
+          }
+          //Intake Code
+          if (m_driveController.getRightBumperButton()) {
+            m_motorFrontIntake.set(Constants.IntakeConstants.k_IntakePower);
+          }
+          if (m_driveController.getLeftBumperButton()) {
+            m_motorBackIntake.set(Constants.IntakeConstants.k_IntakePower);
           }
 
-          if (m_driveController.getAButton() && !m_driveController.getBButton()) {
-            m_motorBackSpin.set(1);
-          } else {
-            m_motorBackSpin.set(0);
-          }
         });
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a
-   * digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
 
+  public double calculateMotorOutput(double radian) {
+    double output = (m_pid.calculate(m_encoder.getPosition().getValueAsDouble())
+     + m_armFeed.calculate(radian, m_encoder.getVelocity().getValueAsDouble()));
+    return output;
+  }
 
 
   @Override
