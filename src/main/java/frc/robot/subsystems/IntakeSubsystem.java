2a5a0c5c3eb294d04aeef2792ef18fbd5f5792b2
henry <henryjordanemail@gmail.com>
2026-02-18 16:26:07 -0800

    2/18 commit

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -6,19 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 import frc.robot.Constants;
-import swervelib.encoders.CanAndMagSwerve;
 
 import com.ctre.phoenix6.hardware.CANcoder;
 
@@ -34,40 +31,40 @@ public class IntakeSubsystem extends SubsystemBase {
 
   private final SparkFlexConfig m_motorConfig = new SparkFlexConfig();
 
-  private final CANcoder m_encoder;
+  private final CANcoder m_frontEncoder;
+  private final CANcoder m_backEncoder;
   private final ArmFeedforward m_armFeed;
+
+  private final int FRONT = 1;
+  private final int BACK = 2;
 
 
  
   public IntakeSubsystem() {
-    m_motorFrontIntake = new SparkFlex(Constants.IntakeConstants.k_FrontIntakeId, null);
-    m_motorFrontSpin = new SparkFlex(Constants.IntakeConstants.k_FrontSpinId, null);
-    m_motorBackIntake = new SparkFlex(Constants.IntakeConstants.k_BackIntakeId, null);
-    m_motorBackSpin = new SparkFlex(Constants.IntakeConstants.k_BackSpinId,null );
-    m_driveController = new XboxController(0);
+    m_motorFrontIntake = new SparkFlex(Constants.IntakeConstants.k_FrontIntakeId, SparkLowLevel.MotorType.kBrushless);
+    m_motorFrontSpin = new SparkFlex(Constants.IntakeConstants.k_FrontSpinId, SparkLowLevel.MotorType.kBrushless);
+    m_motorBackIntake = new SparkFlex(Constants.IntakeConstants.k_BackIntakeId, SparkLowLevel.MotorType.kBrushless);
+    m_motorBackSpin = new SparkFlex(Constants.IntakeConstants.k_BackSpinId,SparkLowLevel.MotorType.kBrushless);
+    m_driveController = new XboxController(Constants.OperatorConstants.kHelperControllerPort);
     m_pid = new PIDController(Constants.IntakeConstants.k_Kp, 0, Constants.IntakeConstants.k_Kd);
-    m_encoder = new CANcoder(0);
+    m_frontEncoder = new CANcoder(Constants.IntakeConstants.k_frontEncoderId);
+    m_backEncoder = new CANcoder(Constants.IntakeConstants.k_BackEncoderId);
     m_armFeed = new ArmFeedforward(Constants.IntakeConstants.k_Ks, Constants.IntakeConstants.k_Kg, Constants.IntakeConstants.k_Kv);
   }
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */ 
+
   public Command runCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
     return run(
         () -> {
           //Spin Motor Code
-          if (m_driveController.getBButton() && !m_driveController.getAButton()){
-            m_motorFrontSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/180));
+          if (m_driveController.getBButton() &&
+           m_backEncoder.getPosition().getValueAsDouble() < Constants.IntakeConstants.k_EncoderThreshold){
+            m_motorFrontSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/180, FRONT));
           
             /*if (m_encoder.getPosition().getValueAsDouble() >= 0.25) {
               m_motorFrontSpin.set(0);
             }*/
           } else {
-            m_motorFrontSpin.set(calculateMotorOutput(0));
+            m_motorFrontSpin.set(calculateMotorOutput(0, FRONT));
                       }
           /* 
           if (m_driveController.getAButton() && !m_driveController.getBButton()) {
@@ -76,10 +73,11 @@ public class IntakeSubsystem extends SubsystemBase {
             m_motorBackSpin.set(-Constants.IntakeConstants.k_SpinPower);
           }
           */
-          if (m_driveController.getAButton() && !m_driveController.getBButton()){
-            m_motorBackSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/ 180));
+          if (m_driveController.getAButton() &&
+           m_frontEncoder.getPosition().getValueAsDouble() < Constants.IntakeConstants.k_EncoderThreshold){
+            m_motorBackSpin.set(calculateMotorOutput((Constants.IntakeConstants.k_TargetAngle * Math.PI)/ 180, BACK));
           } else {
-            m_motorBackSpin.set(calculateMotorOutput(0));
+            m_motorBackSpin.set(calculateMotorOutput(0, BACK));
           }
           //Intake Code
           if (m_driveController.getRightBumperButton()) {
@@ -92,21 +90,25 @@ public class IntakeSubsystem extends SubsystemBase {
         });
   }
 
-
-  public double calculateMotorOutput(double radian) {
-    double output = (m_pid.calculate(m_encoder.getPosition().getValueAsDouble())
-     + m_armFeed.calculate(radian, m_encoder.getVelocity().getValueAsDouble()));
+  //Creates the output needed for the motor spin to a certain radian
+  public double calculateMotorOutput(double radian, int encoder) {
+    double output = 0;
+    if (encoder == FRONT){
+      output = (m_pid.calculate(m_frontEncoder.getPosition().getValueAsDouble())
+     + m_armFeed.calculate(radian, m_frontEncoder.getVelocity().getValueAsDouble()));
+    } else if(encoder == BACK){
+      output = (m_pid.calculate(m_backEncoder.getPosition().getValueAsDouble())
+     + m_armFeed.calculate(radian, m_backEncoder.getVelocity().getValueAsDouble()));
+    }
     return output;
   }
 
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
